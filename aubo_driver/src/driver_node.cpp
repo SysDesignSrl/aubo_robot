1345e69911827a8358ecca383580d89656b9cfc8
Nicola Conti <nicola@sysdesign.it>
2020-03-20 21:45:57 +0100

    publish tool pose sent by robot

--- aubo_driver/src/driver_node.cpp
+++ aubo_driver/src/driver_node.cpp
@@ -8,6 +8,8 @@
 #include <ros/console.h>
 // std_srvs
 #include <std_srvs/Trigger.h>
+// geometry_msgs
+#include <geometry_msgs/PoseStamped.h>
 // sensor_msgs
 #include <sensor_msgs/JointState.h>
 //
@@ -22,15 +24,41 @@ std::vector<std::string> joint_names;
 
 // Published Topics
 ros::Publisher joint_state_pub;
+ros::Publisher tool_pose_pub;
 
 
 void realtime_waypoint_cb(const aubo_robot_namespace::wayPoint_S *wayPoint, void *arg)
 {
+  ros::Time stamp = ros::Time::now();
+
+  double x, y, z;
+  x = wayPoint->cartPos.position.x;
+  y = wayPoint->cartPos.position.y;
+  z = wayPoint->cartPos.position.z;
+
+  double qx, qy, qz, qw;
+  qx = wayPoint->orientation.x;
+  qy = wayPoint->orientation.y;
+  qz = wayPoint->orientation.z;
+  qw = wayPoint->orientation.w;
+
+  geometry_msgs::PoseStamped tool_pose;
+  tool_pose.header.stamp = stamp;
+  tool_pose.header.frame_id = "base_link";
+  tool_pose.pose.position.x = x;
+  tool_pose.pose.position.y = y;
+  tool_pose.pose.position.z = z;
+  tool_pose.pose.orientation.x = qx;
+  tool_pose.pose.orientation.y = qy;
+  tool_pose.pose.orientation.z = qz;
+  tool_pose.pose.orientation.w = qw;
+  tool_pose_pub.publish(tool_pose);
+
   std::vector<double> joint_pos(aubo_robot_namespace::ARM_DOF);
   std::copy(wayPoint->jointpos, wayPoint->jointpos + aubo_robot_namespace::ARM_DOF, joint_pos.begin());
 
   sensor_msgs::JointState joint_state;
-  joint_state.header.stamp = ros::Time::now();
+  joint_state.header.stamp = stamp;
   joint_state.name = joint_names;
   joint_state.position = joint_pos;
   joint_state_pub.publish(joint_state);
@@ -62,6 +90,7 @@ int main(int argc, char* argv[])
 
   // Published Topics
   joint_state_pub = node.advertise<sensor_msgs::JointState>("joint_states", 100);
+  tool_pose_pub = node.advertise<geometry_msgs::PoseStamped>("tool_pose", 100);
 
 
   // AUBO Robot
